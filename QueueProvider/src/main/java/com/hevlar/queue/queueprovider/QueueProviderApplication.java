67e22c5ef30dd3739b08c4e5cd40e9646b0b5dd8
Dennis Cai <dennis@thecodinganalyst.com>
2023-07-29 11:01:21 +0800

    add @EnableDiscoveryClient to the QueueProviderApplication

--- QueueProvider/src/main/java/com/hevlar/queue/queueprovider/QueueProviderApplication.java
+++ QueueProvider/src/main/java/com/hevlar/queue/queueprovider/QueueProviderApplication.java
@@ -2,12 +2,14 @@ package com.hevlar.queue.queueprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 public class QueueProviderApplication {
 
